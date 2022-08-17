568d8e2334d00e6624a24c3c6b9cb936401ba0a0
juanf1328 <jforni@encodesa.com.ar>
2022-08-17 12:16:55 -0300

    half

--- countryApp/src/app/pais/pages/paises/paises.component.ts
+++ countryApp/src/app/pais/pages/paises/paises.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-paises',
@@ -8,13 +11,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PaisesComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+
+  pais!:Country;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private PaisService: PaisService
+    ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params
-    .subscribe( ({id}) =>{
-      console.log(id);
-    })
-  }
+    .pipe(
+      switchMap(({id}) => this.PaisService.getPaisPorAlpha(id)),
+      tap(console.log)
+    )
 
+    .subscribe(pais => this.pais = pais);
+   }
 }
